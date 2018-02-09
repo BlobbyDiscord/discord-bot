1a63e4a33b5449202626956d6c9bcc907aa695e7
Synplex <shane@synplex.co.uk>
2018-02-09 18:27:54 +0000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -19,7 +19,7 @@ exports.run = async (client, message) => {
       let member = message.mentions.users.first();
       const embed = new Discord.RichEmbed()
       .setTitle(`Discord profile for ${member.username}`)
-      .setThumbnail(`${member.avatarURL}`)
+      .setThumbnail(`${member.avatarURL}` || 'https://camo.githubusercontent.com/1d25b77d1fb7e3f24fe2ef0063effe1981cb3f9c/687474703a2f2f6973322e6d7a7374617469632e636f6d2f696d6167652f7468756d622f507572706c65332f76342f65332f39642f32332f65333964323339652d376237612d653362372d633762662d3163313630653937633238632f6d7a6c2e646e6563666b6e702e706e672f30783073732d38352e6a7067')
       .setDescription(
         "Interested to see how you fair against your fellow members?\n",
       )
